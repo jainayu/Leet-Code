5808b6ea07b6f2296a62b0c6e4b104fdd0e6dadf
jainayu <500060884@stu.upes.ac.in>
2020-08-10 08:06:08 +0530

    Min Stack

---
+++ Top Interview Questions/Easy Collection/Design/MinStack.cpp
@@ -0,0 +1,43 @@
+class MinStack {
+    stack <int> s;
+    int min = INT_MAX;
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        if(x <= min){
+            s.push(min);
+            min = x;
+        }
+        s.push(x);
+    }
+    
+    void pop() {
+        int top = s.top();
+        s.pop();
+        if(top == min) {
+            min = s.top();
+            s.pop();
+        }
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return min;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
